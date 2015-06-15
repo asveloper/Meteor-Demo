c41d568b160aee5350a950902df354eb02d687c5
Arslan Nasir <arslan.nasir92@gmail.com>
2015-06-15 16:48:21 +0500

    Facebook requested permissions updated

--- client/templates/pages/welcome.js
+++ client/templates/pages/welcome.js
@@ -1,6 +1,7 @@
 Template.PagesWelcome.events({
   "click #facebookLogin": function (event) {
       Meteor.loginWithFacebook({
+        requestPermissions: ['email', 'public_profile']
       }, function (err) {
         if (err)
           Session.set('errorMessage', err.reason || 'Unknown error');
