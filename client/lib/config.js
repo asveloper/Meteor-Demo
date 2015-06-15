24c4973cebf3d597b71e3f81d85556fc5b42d107
Arslan Nasir <arslan.nasir92@gmail.com>
2015-06-15 16:23:34 +0500

    Facebook app config added

---
+++ client/lib/config.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.upsert(
+  { service: "facebook" },
+  {
+    $set: {
+      clientId: "479744148860467",
+      loginStyle: "popup",
+      secret: "24800f528a109e90e06aa9f45fdc6cb9"
+    }
+  }
+);
