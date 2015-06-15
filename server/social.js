185c0a359c985e05efdf7fa9b3e5935090219257
Arslan Nasir <arslan.nasir92@gmail.com>
2015-06-15 16:48:03 +0500

    Social config updated

--- server/social.js
+++ server/social.js
@@ -1,10 +1,9 @@
-ServiceConfiguration.configurations.upsert(
-  { service: "facebook" },
-  {
-    $set: {
-      clientId: "479744148860467",
-      loginStyle: "popup",
-      secret: "24800f528a109e90e06aa9f45fdc6cb9"
-    }
-  }
-);
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '479744148860467',
+    secret: '24800f528a109e90e06aa9f45fdc6cb9'
+});
