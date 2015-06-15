d6b27bf2bb75d194554abf22b84b2a813dce0d12
Arslan Nasir <arslan.nasir92@gmail.com>
2015-06-15 17:56:00 +0500

    Dashboard templates updated

--- client/templates/pages/dashboard.js
+++ client/templates/pages/dashboard.js
@@ -1,7 +1,49 @@
 Template.PagesDashboard.helpers({
+  referralUrl: function(){
+    var referralCode = "";
 
+    if(Meteor.user()){
+      referralCode = Meteor.user().profile.referralCode;
+    }
+    return Router.current().url.replace("dashboard", "?ref="+referralCode);
+  },
+  shareData: function(){
+    var referralCode = "";
+
+    if(Meteor.user()){
+      referralCode = Meteor.user().profile.referralCode;
+    }
+    return {
+      url: Router.current().url.replace("dashboard", "?ref="+referralCode)
+    }
+  },
+  level: function(){
+    var referralCode = "";
+
+    if(Meteor.user()){
+      referralCode = Meteor.user().profile.referralCode;
+    }
+
+    return ;
+  }
 });
 
 Template.PagesDashboard.events({
+  "click #logout": function(){
+    Meteor.logout();
+    Router.go('/');
+  }
 
 });
+
+Template.PagesDashboard.rendered = function(){
+
+    if(Session.get("referrerCode")){
+      Meteor.users.update({_id: Meteor.userId()}, {
+        $set: {
+          referrerCode: Session.get("referrerCode")
+        }
+      });
+    }
+
+}
