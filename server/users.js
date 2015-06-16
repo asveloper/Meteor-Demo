4381efd75d1a483051a7ddb0723539882917865a
Arslan Nasir <arslan.nasir92@gmail.com>
2015-06-16 20:51:49 +0500

    Referral Count publication updated

--- server/users.js
+++ server/users.js
@@ -1,4 +1,6 @@
 Meteor.publish("referralCount", function(){
-    var referralCode = Meteor.users.findOne({_id: this.userId}).profile.referralCode;
-    Counts.publish(this, 'referralCount', Meteor.users.find({"profile.referrerCode": referralCode}));
+  var user = Meteor.users.findOne({_id: this.userId});
+  if(user){
+    Counts.publish(this, 'referralCount', Meteor.users.find({"profile.referrerCode": user.profile.referralCode}));
+  }
 });
